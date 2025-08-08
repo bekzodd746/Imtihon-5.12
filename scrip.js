d33bd7d5589774e1a3ebc36e88bfd547aa586c95
bekzodd746 <bekzodxoliqov746@gmail.com>
2025-08-08 12:57:13 +0300

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,47 @@
+// 2-savol
+
+let a = Math.trunc(Math.random()*10)
+
+alert(a);
+
+// 3-sovol
+
+let b = prompt('write number')
+console.log(Math.trunc(b));
+
+// 5=savol
+
+// let data = ['MARS IT SCHOOL']
+// console.log(data);
+// for(i=0;i<data.5 i++){
+//     console.log(data(i));
+    
+// }
+
+// 7-savol
+
+let c = Math.trunc(Math.random()*50)
+
+alert(c)
+
+if(a > 18){
+    console.log('katta');
+    
+}else{
+    console.log('kichik');
+    
+}
+
+// 9-savol
+
+let d = prompt('write something')
+
+let h1 = document.querySelector('h1')
+
+h1.textContent=d
+
+h1.style.color = 'red'
+
+// 10-savol
+
+let 
